22b5bc5f1f97ce97fa584fee1117d20f1a909c9a
Danilo Amado <danilo_amado@yahoo.com>
2026-01-24 17:37:58 -0500

    ID must have an assigned value before invoking EntityManager.persist(entity)

--- src/main/java/com/example/demo/model/Role.java
+++ src/main/java/com/example/demo/model/Role.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -12,6 +14,7 @@ import lombok.Setter;
 public class Role {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "The role name is required")
