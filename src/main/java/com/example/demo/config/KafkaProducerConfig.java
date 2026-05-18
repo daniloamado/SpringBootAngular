a915093530564d819d9fe840f06014d65c592801
Danilo Amado <danilo_amado@yahoo.com>
2026-05-18 18:44:41 -0400

    Commenting out Kafka Topic and Producer so that I don't need to run Kafka all the time

--- src/main/java/com/example/demo/config/KafkaProducerConfig.java
+++ src/main/java/com/example/demo/config/KafkaProducerConfig.java
@@ -1,37 +1,25 @@
 package com.example.demo.config;
 
-import com.example.demo.model.User;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
 
-    @Value(value = "${spring.kafka.bootstrap-servers}")
-    private String bootstrapAddress;
-
-    @Bean
-    public ProducerFactory<String, User> producerFactoryEnrolment() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress); // kafka admin bean
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
-    }
-
-    @Bean
-    public KafkaTemplate<String, User> kafkaTemplateEnrolment() {
-        return new KafkaTemplate<>(producerFactoryEnrolment());
-    }
+//    @Value(value = "${spring.kafka.bootstrap-servers}")
+//    private String bootstrapAddress;
+//
+//    @Bean
+//    public ProducerFactory<String, User> producerFactoryEnrolment() {
+//        Map<String, Object> configProps = new HashMap<>();
+//        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress); // kafka admin bean
+//        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+//        return new DefaultKafkaProducerFactory<>(configProps);
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, User> kafkaTemplateEnrolment() {
+//        return new KafkaTemplate<>(producerFactoryEnrolment());
+//    }
 
 }
