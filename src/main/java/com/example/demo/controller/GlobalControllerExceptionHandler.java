ec77019acc94b29e28861b763a4a4e4891c1accd
Danilo Amado <danilo_amado@yahoo.com>
2026-05-18 18:43:31 -0400

    Response status improved

--- src/main/java/com/example/demo/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/com/example/demo/controller/GlobalControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.RecordNotFoundException;
 import com.example.demo.model.ResponseStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.Instant;
 
 @RestControllerAdvice
 public class GlobalControllerExceptionHandler {
@@ -20,13 +20,26 @@ public class GlobalControllerExceptionHandler {
 
         ResponseStatus responseStatus = new ResponseStatus();
         responseStatus.setStatus(HttpStatus.BAD_REQUEST.value());
-        responseStatus.setMessage("Validation Failed");
+        responseStatus.setTitle("Validation Failed");
+        responseStatus.setTimestamp(Instant.now().toString());
 
         e.getBindingResult().getFieldErrors().forEach(error ->
                 responseStatus.getErrors().put(error.getField(), error.getDefaultMessage())
         );
 
         return new ResponseEntity<>(responseStatus, HttpStatus.BAD_REQUEST);
+    }
+
+    @org.springframework.web.bind.annotation.ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(RecordNotFoundException.class)
+    public ResponseEntity<ResponseStatus> handleException(RecordNotFoundException e, WebRequest request){
+
+        ResponseStatus responseStatus = new ResponseStatus();
+        responseStatus.setStatus(HttpStatus.NOT_FOUND.value());
+        responseStatus.setTimestamp(Instant.now().toString());
+        responseStatus.setTitle(e.getMessage());
+
+        return new ResponseEntity<>(responseStatus, HttpStatus.NOT_FOUND);
     }
 
 }
--- src/main/java/com/example/demo/model/ResponseStatus.java
+++ src/main/java/com/example/demo/model/ResponseStatus.java
@@ -1,15 +1,18 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ResponseStatus {
 
     private int status;
-    private String message;
+    private String title;
+    private String timestamp;
     private Map<String, String> errors = new HashMap<>();
 
 }
